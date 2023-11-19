8aacb6b41982af4165458b6b0a3fb81ac3b46ee8
Rajeshkumarachari <131753283+Rajeshkumarachari@users.noreply.github.com>
2023-11-19 21:05:37 +0530

    new update

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,11 +1,28 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { LEFT_DESIGN } from "../Utils/constant";
 
 const Question = () => {
   const [show, setShow] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [close, setClose] = useState(false);
+  const [make, setMake] = useState(false);
+  const [poll, setPoll] = useState(false);
 
-  const changeToggle = () => {
+  const onChange = () => {
     setShow(!show);
+  };
+  const onPark = () => {
+    setOpen(!open);
+  };
+
+  const advance = () => {
+    setClose(!close);
+  };
+  const mass = () => {
+    setMake(!make);
+  };
+  const shadow = () => {
+    setPoll(!poll);
   };
 
   return (
@@ -14,13 +31,12 @@ const Question = () => {
         Frequently Asked Questions
       </h2>
       <div
-        onClick={changeToggle}
-        className="flex border shadow-lg  text-center h-10 pl-1/3 my-6 cursor-pointer"
+        onClick={onChange}
+        className="flex border shadow-lg mx-96  text-center h-10 pl-1/3 my-6 cursor-pointer"
       >
-        <p className=" text-center pl-[600px] text-lg">
-          Can I reserve a parking spot in advance?
+        <p className=" text-center pl-48 text-lg">
+          Can I reserve a parking spot in advance? {show ? "-" : "+"}
         </p>
-        <img className=" w-5" src={LEFT_DESIGN} alt="" />
       </div>
       {show ? (
         <div>
@@ -30,14 +46,15 @@ const Question = () => {
           </p>
         </div>
       ) : null}
-
-      <div className="flex border shadow-lg  text-center h-10 pl-1/3 my-6 cursor-pointer">
-        <p className=" text-center pl-[600px] text-lg">
-          Can I cancel my parking reservation?
+      <div
+        onClick={onPark}
+        className="flex border shadow-lg mx-96  text-center h-10 pl-1/3 my-6 cursor-pointer"
+      >
+        <p className=" text-center pl-48 text-lg">
+          Can I cancel my parking reservation? {open ? "-" : "+"}
         </p>
-        <img className=" w-5" src={LEFT_DESIGN} alt="" />
       </div>
-      {show ? (
+      {open ? (
         <div>
           <p className=" text-lg">
             Yes, you can cancel your parking reservation before your allotted{" "}
@@ -46,57 +63,52 @@ const Question = () => {
           </p>
         </div>
       ) : null}
-
       <div
-        onClick={changeToggle}
-        className="flex border shadow-lg  text-center h-10 pl-1/3 my-6 cursor-pointer"
+        onClick={advance}
+        className="flex border shadow-lg mx-96 text-center h-10 pl-1/3 my-6 cursor-pointer"
       >
-        <p className=" text-center pl-[600px] text-lg">
-          Can I reserve a parking spot in advance?
+        <p className=" text-center pl-48 text-lg">
+          Does ParkQwik operate in multiple cities? {close ? "-" : "+"}
         </p>
-        <img className=" w-5" src={LEFT_DESIGN} alt="" />
       </div>
-      {show ? (
+      {close ? (
         <div>
           <p className=" text-lg">
-            Yes, you can book your parking space in advance. You can choose{" "}
-            <br /> the date and time when you need your parking space.
+            Yes, we operate in all metro cities and the cities around them.
+          </p>
+        </div>
+      ) : null}
+      <div
+        onClick={mass}
+        className="flex border shadow-lg mx-96 text-center h-10 pl-1/3 my-6 cursor-pointer"
+      >
+        <p className=" text-center pl-48 text-lg">
+          How can I extend my parking reservation ? {make ? "-" : "+"}
+        </p>
+      </div>
+      {make ? (
+        <div>
+          <p className=" text-lg">
+            You can either rebook or contact customer support to extend your
+            parking.
           </p>
         </div>
       ) : null}
 
       <div
-        onClick={changeToggle}
-        className="flex border shadow-lg  text-center h-10 pl-1/3 my-6 cursor-pointer"
+        onClick={shadow}
+        className="flex border shadow-lg mx-96  text-center h-10 pl-1/3 my-6 cursor-pointer"
       >
-        <p className=" text-center pl-[600px] text-lg">
-          Can I reserve a parking spot in advance?
+        <p className=" text-center pl-48 text-lg">
+          Do you offer valet parking service? {poll ? "-" : "+"}
         </p>
-        <img className=" w-5" src={LEFT_DESIGN} alt="" />
       </div>
-      {show ? (
+      {poll ? (
         <div>
           <p className=" text-lg">
-            Yes, you can book your parking space in advance. You can choose{" "}
-            <br /> the date and time when you need your parking space.
-          </p>
-        </div>
-      ) : null}
-
-      <div
-        onClick={changeToggle}
-        className="flex border shadow-lg  text-center h-10 pl-1/3 my-6 cursor-pointer"
-      >
-        <p className=" text-center pl-[600px] text-lg">
-          Can I reserve a parking spot in advance?
-        </p>
-        <img className=" w-5" src={LEFT_DESIGN} alt="" />
-      </div>
-      {show ? (
-        <div>
-          <p className=" text-lg">
-            Yes, you can book your parking space in advance. You can choose{" "}
-            <br /> the date and time when you need your parking space.
+            Yes, we do offer valet parking services. You can reach out to our{" "}
+            <br />
+            customer support number at 842 842 88 33 to book your valet parking.
           </p>
         </div>
       ) : null}
